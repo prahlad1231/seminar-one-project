06be85dcd4325bc827369b4a70c8b9edcec69724
Prahlad Panthi <prahladpanthi@Prahlads-MacBook-Pro.local>
2023-11-05 19:40:59 +1100

    initial commit for seminar one project

---
+++ seminarone-backend/seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/SeminaroneBackendApplication.java
@@ -0,0 +1,13 @@
+package np.com.prahladpanthi.seminaronebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminaroneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeminaroneBackendApplication.class, args);
+    }
+
+}
---
+++ seminarone-backend/seminarone-backend/src/test/java/np/com/prahladpanthi/seminaronebackend/SeminaroneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.prahladpanthi.seminaronebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminaroneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
