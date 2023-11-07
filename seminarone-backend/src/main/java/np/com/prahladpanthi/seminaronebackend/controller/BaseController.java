e62b1451d6129999575eb792433401bbc36d0c4a
prahlad1231 <prahladpanthi.work@gmail.com>
2023-11-08 08:50:57 +1100

    added controller for seminar

--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/BaseController.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/BaseController.java
@@ -1,2 +1,7 @@
-package np.com.prahladpanthi.seminaronebackend.controller;public class BaseController {
+package np.com.prahladpanthi.seminaronebackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
 }
--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/SeminarController.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/SeminarController.java
@@ -1,2 +1,50 @@
-package np.com.prahladpanthi.seminaronebackend.controller;public class SeminarController {
+package np.com.prahladpanthi.seminaronebackend.controller;
+
+import np.com.prahladpanthi.seminaronebackend.dto.ResponseDto;
+import np.com.prahladpanthi.seminaronebackend.dto.SeminarDto;
+import np.com.prahladpanthi.seminaronebackend.entity.SeminarEntity;
+import np.com.prahladpanthi.seminaronebackend.exception.InsufficientDataException;
+import np.com.prahladpanthi.seminaronebackend.mapper.SeminarMapper;
+import np.com.prahladpanthi.seminaronebackend.service.ISeminarService;
+import np.com.prahladpanthi.seminaronebackend.util.APIConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(APIConstants.SEMINAR)
+public class SeminarController extends BaseController {
+
+    private ISeminarService seminarService;
+    private SeminarMapper seminarMapper;
+
+    @Autowired
+    public SeminarController(ISeminarService seminarService, SeminarMapper seminarMapper) {
+        this.seminarService = seminarService;
+        this.seminarMapper = seminarMapper;
+    }
+
+    @GetMapping(APIConstants.FIND_ALL)
+    public ResponseEntity<ResponseDto> findAll() {
+        return new ResponseEntity<>(new ResponseDto("Successfully fetched!", seminarService.findAll()), HttpStatus.OK);
+    }
+
+    @GetMapping(APIConstants.FIND_BY_ID)
+    public ResponseEntity<ResponseDto> findById(@PathVariable("id") Long id) {
+        final SeminarEntity seminarEntity = seminarService.findById(id);
+        return new ResponseEntity<>(new ResponseDto("Successfully fetched!", seminarMapper.mapToDto(seminarEntity)), HttpStatus.OK);
+    }
+
+    @PostMapping(APIConstants.SAVE)
+    public ResponseEntity<ResponseDto> save(@RequestBody SeminarDto seminarDto) {
+        if (!(seminarDto.getTitle().isBlank() && seminarDto.getStartDate() == null &&
+                seminarDto.getEndDate() == null && seminarDto.getLocationEntityId() == null &&
+                seminarDto.getTopicEntityId() == null)) {
+            throw new InsufficientDataException("Please enter all the details for adding seminar!");
+        }
+
+        SeminarEntity seminarEntity = seminarService.save(seminarMapper.mapToEntity(seminarDto));
+        return new ResponseEntity<>(new ResponseDto("Successfully saved!", seminarMapper.mapToDto(seminarEntity)), HttpStatus.CREATED);
+    }
 }
