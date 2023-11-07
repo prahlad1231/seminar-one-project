7549765f916e2806287ac4d53de8d0eb34cd7e9f
prahlad1231 <prahladpanthi.work@gmail.com>
2023-11-08 08:50:31 +1100

    added api constants

--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/util/APIConstants.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/util/APIConstants.java
@@ -1,2 +1,20 @@
-package np.com.prahladpanthi.seminaronebackend.util;public interface APIConstants {
+package np.com.prahladpanthi.seminaronebackend.util;
+
+public interface APIConstants {
+
+    String BASE_API = "/api/seminarone";
+
+    /****************** COMMON ****************/
+
+    String FIND_ALL = "/findAll";
+    String FIND_BY_ID = "/find/{id}";
+    String SAVE = "/save";
+    String UPDATE = "/update";
+    String UPDATE_BY_ID = "/update/{id}";
+    String DELETE = "/delete";
+    String DELETE_BY_ID = "/delete/{id}";
+
+    /****************** APIs FOR SEMINAR CONTROLLER ****************/
+
+    String SEMINAR = BASE_API + "/seminar";
 }
