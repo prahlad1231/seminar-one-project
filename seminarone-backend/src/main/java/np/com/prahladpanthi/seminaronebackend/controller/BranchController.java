65702a9f807d6698907ee8436fcd80281dd5d7a1
prahlad1231 <prahladpanthi.work@gmail.com>
2023-11-10 23:18:06 +1100

    added empty controllers

---
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/BranchController.java
@@ -0,0 +1,7 @@
+package np.com.prahladpanthi.seminaronebackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BranchController extends BaseController {
+}
---
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/LocationController.java
@@ -0,0 +1,7 @@
+package np.com.prahladpanthi.seminaronebackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController extends BaseController {
+}
