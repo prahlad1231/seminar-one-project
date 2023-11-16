5d3d72b8ce76ca3bb957220a88a54788a4940d64
prahlad1231 <prahladpanthi.work@gmail.com>
2023-11-17 02:13:43 +1100

    added a shared component for displaying headings

--- seminarone-frontend/src/components/AddSeminar.js
+++ seminarone-frontend/src/components/AddSeminar.js
@@ -1,5 +1,11 @@
+import FormHeading from "./shared/FormHeading";
+
 const AddSeminar = () => {
-  return <div>Add Seminar</div>;
+  return (
+    <div className="addseminar">
+      <FormHeading title="Add New Seminar" />
+    </div>
+  );
 };
 
 export default AddSeminar;
