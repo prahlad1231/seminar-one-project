0074cc4672675497f0fc522bebcfdcad4243fa59
prahlad1231 <prahladpanthi.work@gmail.com>
2023-11-17 22:52:38 +1100

    added framework for menu

--- seminarone-frontend/src/components/AddSeminar.js
+++ seminarone-frontend/src/components/AddSeminar.js
@@ -1,9 +1,17 @@
 import FormHeading from "./shared/FormHeading";
 
+import "../styles/addseminar.css";
+import { Paper } from "@mui/material";
+
 const AddSeminar = () => {
   return (
-    <div className="addseminar">
+    <div className="seminar-container">
       <FormHeading title="Add New Seminar" />
+      <div className="seminar-form">
+        <Paper elevation={2} square={false} sx={{ padding: "1rem" }}>
+          Paper
+        </Paper>
+      </div>
     </div>
   );
 };
