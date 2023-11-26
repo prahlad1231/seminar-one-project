d8d2ede02e4a9389ad7d8cd916b46e5101ce6ac1
prahlad1231 <prahladpanthi.work@gmail.com>
2023-11-26 18:32:17 +1100

    added some services to add and get locations and topics of a seminar

---
+++ seminarone-frontend/src/services/SeminarService.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_SEMINAR_ONE_BASE_API;
+
+class SeminarService {
+  getAllSeminars() {
+    return axios({
+      method: "get",
+      url: API_URL + "seminar/findAll",
+    });
+  }
+
+  getSeminarById(seminarId) {
+    return axios({
+      method: "get",
+      url: API_URL + `seminar/find/${seminarId}`,
+    });
+  }
+}
+
+class LocationService {
+  getAllVenues() {
+    return axios({
+      method: "get",
+      url: API_URL + "location/findAll",
+    });
+  }
+
+  getVenueById(venueId) {
+    return axios({
+      method: "get",
+      url: API_URL + `location/find/${venueId}`,
+    });
+  }
+
+  save(locationDetails) {
+    return axios({
+      method: "POST",
+      url: API_URL + "location/save",
+      data: locationDetails,
+    });
+  }
+}
+
+class TopicService {
+  getAllTopics() {
+    return axios({
+      method: "get",
+      url: API_URL + "topic/findAll",
+    });
+  }
+
+  getTopicById(topicId) {
+    return axios({
+      method: "get",
+      url: API_URL + `topic/find/${topicId}`,
+    });
+  }
+
+  save(topicDetails) {
+    return axios({
+      method: "POST",
+      url: API_URL + "topic/save",
+      data: topicDetails,
+    });
+  }
+}
+
+export { SeminarService, LocationService, TopicService };
