f3043fdbcef2d6f0b2f2ba21a5a76951dbab334b
prahlad1231 <prahladpanthi.work@gmail.com>
2023-12-12 23:49:32 +1100

    add basic paper layout in settings

--- seminarone-frontend/src/components/Settings.js
+++ seminarone-frontend/src/components/Settings.js
@@ -1,7 +1,14 @@
+import { Paper } from "@mui/material";
+
 const Settings = () => {
   return (
     <div>
       <h2>Settings</h2>
+      <div className="settings">
+        <Paper elevation={3} className="settings-paper">
+          <h3>Content</h3>
+        </Paper>
+      </div>
     </div>
   );
 };
