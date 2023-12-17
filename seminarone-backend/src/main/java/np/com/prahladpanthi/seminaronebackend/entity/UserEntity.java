27839ab80088ef09ec1aa1f6ca58618392e5b19b
prahlad1231 <prahladpanthi.work@gmail.com>
2023-12-18 02:01:10 +1100

    add many to many relation with roles entity

--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/entity/UserEntity.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/entity/UserEntity.java
@@ -1,13 +1,14 @@
 package np.com.prahladpanthi.seminaronebackend.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,13 +23,17 @@ public class UserEntity extends BaseEntity{
     @Column(name = "last_name")
     private String lastName;
 
+    @NotNull
+    @Column(name = "username", unique = true)
     private String username;
 
+    @NotNull
     private String password;
 
     @Column(name = "postal_code")
     private String postalCode;
 
+    @NotNull
     private String email;
 
     @Column(name = "credit_card_number")
@@ -36,5 +41,10 @@ public class UserEntity extends BaseEntity{
 
     @Column(name = "expiry_date")
     private Date expiryDate;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<RoleEntity> roleEntityList = new ArrayList<>();
 
 }
