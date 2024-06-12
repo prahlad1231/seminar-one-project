866cb3c7c88281166211b389c3a94c344c305fcc
prahlad1231 <prahladpanthi.work@gmail.com>
2024-06-12 22:31:40 +1000

    added preauthorize while changing password

--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/UserController.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/UserController.java
@@ -6,6 +6,7 @@ import np.com.prahladpanthi.seminaronebackend.service.IUserService;
 import np.com.prahladpanthi.seminaronebackend.util.APIConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(APIConstants.USER)
+@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
 public class UserController extends BaseController {
 
     private final IUserService userService;
