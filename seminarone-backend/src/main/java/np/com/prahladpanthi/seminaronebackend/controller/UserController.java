9dcbb6f56ed7cba0c119cbcf4bb20f7e2ac6bf1d
prahlad1231 <prahladpanthi.work@gmail.com>
2024-06-25 22:19:10 +1000

    fixed the permission

--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/UserController.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/UserController.java
@@ -2,16 +2,14 @@ package np.com.prahladpanthi.seminaronebackend.controller;
 
 import np.com.prahladpanthi.seminaronebackend.dto.ResponseDto;
 import np.com.prahladpanthi.seminaronebackend.dto.custom.ChangePasswordDto;
+import np.com.prahladpanthi.seminaronebackend.mapper.UserMapper;
 import np.com.prahladpanthi.seminaronebackend.service.IUserService;
 import np.com.prahladpanthi.seminaronebackend.util.APIConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(APIConstants.USER)
@@ -20,11 +18,22 @@ public class UserController extends BaseController {
 
     private final IUserService userService;
 
-    public UserController(IUserService userService) {
+    private final UserMapper userMapper;
+
+    public UserController(IUserService userService, UserMapper userMapper) {
         super(userService);
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping(APIConstants.FIND_ALL)
+    public ResponseEntity<ResponseDto> getAllUsers() {
+        return new ResponseEntity<>(new ResponseDto("Successfully fetched!",
+                userMapper.mapToDto(userService.findAll())), HttpStatus.OK);
+    }
+
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @PostMapping(APIConstants.CHANGE_PASSWORD)
     public ResponseEntity<ResponseDto> changePassword(Authentication authentication,
                                                       @RequestBody ChangePasswordDto changePasswordDto) {
--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/service/impl/UserServiceImpl.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/service/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package np.com.prahladpanthi.seminaronebackend.service.impl;
 
-import lombok.RequiredArgsConstructor;
 import np.com.prahladpanthi.seminaronebackend.dto.custom.ChangePasswordDto;
 import np.com.prahladpanthi.seminaronebackend.entity.UserEntity;
 import np.com.prahladpanthi.seminaronebackend.exception.InsufficientDataException;
@@ -9,15 +8,21 @@ import np.com.prahladpanthi.seminaronebackend.repository.UserRepository;
 import np.com.prahladpanthi.seminaronebackend.service.IUserService;
 import np.com.prahladpanthi.seminaronebackend.service.impl.base.BaseServiceImpl;
 import np.com.prahladpanthi.seminaronebackend.util.BCryptUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class UserServiceImpl extends BaseServiceImpl<UserEntity, Long> implements IUserService {
 
     private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        super(userRepository);
+        this.userRepository = userRepository;
+    }
 
     @Override
     public Optional<UserEntity> findByUsername(String username) {
