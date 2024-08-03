932adf32a9a68f5e53d8c2f6fa521983162c1e6f
prahlad1231 <prahladpanthi.work@gmail.com>
2024-08-03 21:12:12 +1000

    add: update the user details

--- seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/UserController.java
+++ seminarone-backend/src/main/java/np/com/prahladpanthi/seminaronebackend/controller/UserController.java
@@ -1,10 +1,14 @@
 package np.com.prahladpanthi.seminaronebackend.controller;
 
 import np.com.prahladpanthi.seminaronebackend.dto.ResponseDto;
+import np.com.prahladpanthi.seminaronebackend.dto.UserDto;
 import np.com.prahladpanthi.seminaronebackend.dto.custom.ChangePasswordDto;
+import np.com.prahladpanthi.seminaronebackend.entity.UserEntity;
+import np.com.prahladpanthi.seminaronebackend.exception.InsufficientDataException;
 import np.com.prahladpanthi.seminaronebackend.mapper.UserMapper;
 import np.com.prahladpanthi.seminaronebackend.service.IUserService;
 import np.com.prahladpanthi.seminaronebackend.util.APIConstants;
+import np.com.prahladpanthi.seminaronebackend.util.BeanCopyUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -31,6 +35,18 @@ public class UserController extends BaseController {
     public ResponseEntity<ResponseDto> getAllUsers() {
         return new ResponseEntity<>(new ResponseDto("Successfully fetched!",
                 userMapper.mapToDto(userService.findAll())), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping(APIConstants.UPDATE)
+    public ResponseEntity<ResponseDto> update(@RequestBody UserDto userDto) {
+        if (userDto.getId() == null) throw new InsufficientDataException("Please provide all details!");
+        UserEntity userEntity = userService.findById(userDto.getId());
+        UserDto updatedUser = userMapper.mapToDto(userEntity);
+        BeanCopyUtils.copyNonNullProperties(userDto, updatedUser);
+
+        userService.update(userMapper.mapToEntity(updatedUser));
+        return new ResponseEntity<>(new ResponseDto("Successfully updated!", updatedUser), HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
