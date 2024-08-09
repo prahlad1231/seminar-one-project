365d61ba2fa5c978e05a0c6bf2552f5b165385f6
prahlad1231 <prahladpanthi.work@gmail.com>
2024-08-09 21:17:39 +1000

    changed to typescript

--- seminarone-frontend/src/services/RequireAuth.tsx
+++ seminarone-frontend/src/services/RequireAuth.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../components/context/AuthContext";
 
-const RequireAuth = ({ children }) => {
+const RequireAuth = ({ children }: any) => {
   const navigate = useNavigate();
   const { isAuthenticated } = useAuth();
 
