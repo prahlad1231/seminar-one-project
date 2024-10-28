9842494af8e66cb71f4fcf233539e298cec893cf
prahlad1231 <prahladpanthi.work@gmail.com>
2024-10-28 14:23:19 +1100

    Initial commit for Vue Project

---
+++ seminarone-vue/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <main>
+    <h1>This is a home page.</h1>
+    <HelloWorld msg="Hello World Component!" />
+  </main>
+</template>
